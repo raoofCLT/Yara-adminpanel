07a9952e5e0a0dc726aeabc87fde4baecb2e70a0
Abdul Raoof <raoofkottayil@gmail.com>
2024-09-24 22:15:13 +0530

    Update files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import Sidebar from "./components/sidebar/Sidebar";
-import Topbar from "./components/topbar/Topbar";
 import "./app.css";
-import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserList from "./pages/userList/UserList";
-import User from "./pages/user/User";
-import NewUser from "./pages/newUser/NewUser";
-import ProductList from "./pages/productList/ProductList";
-import Product from "./pages/product/Product";
-import NewProduct from "./pages/newProduct/NewProduct";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import { PageRoutes } from "./PageRoutes";
 
 function App() {
+  const rootData = JSON.parse(localStorage.getItem("persist:root"));
+  const user =
+    rootData && rootData.user ? JSON.parse(rootData.user).currentUser : null;
+  const isAdmin = user ? user.isAdmin : false;
   return (
-    <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/users" element={<UserList />} />
-          <Route path="/user/:userId" element={<User />} />
-          <Route path="/newUser" element={<NewUser />} />
-          <Route path="/products" element={<ProductList/>} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/newProduct" element={<NewProduct />} />
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      {isAdmin ?(
+        <>
+          <Route path="/*" element={<PageRoutes />} />
+        </>
+      ): <Route path="/login" />}
+      
+        <Route path="/login" element={<Login />} />
+     
+      
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <App />
+  <Provider store={store}>
+    <PersistGate loading="null" persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
